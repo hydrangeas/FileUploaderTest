ae3127d7b9a1a80349cac1bfa7ca7f278bae2a44
Akira Otaka <hydrangeas.project@gmail.com>
2019-11-04 22:32:24 +0900

    テストを追加

---
+++ FileUploaderTest.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FileUploaderTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
